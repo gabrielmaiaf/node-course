ed01c829f99675b543ce6b76dc07477c5e4ea279
Gabriel Maia <gabrielmaia@protonmail.com>
2020-07-09 20:34:34 -0300

    v1

---
+++ module-12.1 - heroku/src/api.js
@@ -0,0 +1,93 @@
+const { config } = require('dotenv');
+const { join } = require('path');
+const { ok } = require('assert');
+
+const env = process.env.NODE_ENV || 'dev';
+ok(env === 'prod' || env === 'dev', 'a env é inválida');
+
+const configPath = join(__dirname, '../config', `.env.${env}`);
+config({
+  path: configPath
+})
+
+const Hapi = require('hapi');
+const HapiSwagger = require('hapi-swagger');
+const Vision = require('vision');
+const Inert = require('inert');
+const HapiJwt = require('hapi-auth-jwt2');
+
+const Context = require('./db/strategies/base/contextStrategy')
+const MongoDB = require('./db/strategies/mongodb/mongodb');
+const Postgres = require('./db/strategies/postgres/postgres');
+const HeroiSchema = require('./db/strategies/mongodb/schemas/heroisSchema');
+const UserSchema = require('./db/strategies/postgres/schemas/userSchema');
+const HeroRoutes = require('./routes/heroRoutes');
+const AuthRoutes = require('./routes/authRoutes');
+
+const JWT_SECRET = process.env.JWT_KEY;
+
+const app = new Hapi.Server({
+  port: process.env.PORT
+});
+
+function mapRoutes(instance, methods) {
+  return methods.map(method => instance[method]());
+}
+
+async function main() {
+  const connectionMDB = MongoDB.connect();
+  const contextMDB = new Context(new MongoDB(connectionMDB, HeroiSchema));
+  const connectionPG = await Postgres.connect();
+  const model = await Postgres.defineModel(connectionPG, UserSchema);
+  const contextPG = new Context(new Postgres(connectionPG, model));
+
+  const swaggerOptions = {
+    info: {
+      title: 'Hero API - #CursoNodeBR',
+      version: 'v1.0'
+    },
+    lang: 'pt'
+  }
+
+  await app.register([
+    HapiJwt,
+    Vision,
+    Inert,
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions 
+    }
+  ]);
+
+  app.auth.strategy('jwt', 'jwt', {
+    key: JWT_SECRET,
+    validate: async (data, request) => {
+      const [result] = await contextPG.read({
+        username: data.username.toLowerCase()
+      })
+      
+      if (!result) {
+        return {
+          isValid: false
+        }
+      }
+
+      return {
+        isValid: true
+      }
+    }
+  })
+  app.auth.default('jwt')
+
+  app.route([
+    ...mapRoutes(new HeroRoutes(contextMDB), HeroRoutes.methods()),
+    ...mapRoutes(new AuthRoutes(JWT_SECRET, contextPG), AuthRoutes.methods())
+  ]);
+
+  await app.start();
+  console.log(`Servidor running at port ${app.info.port}`);
+
+  return app;
+}
+
+module.exports = main();
---
+++ module-12.1 - heroku/src/tests/apiHeroes.test.js
@@ -0,0 +1,166 @@
+const assert = require('assert');
+const api = require('../api');
+let app = {};
+
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imhlcm9hc3NvY2lhdGlvbiIsImlkIjoxLCJpYXQiOjE1OTQzMDk5NTJ9.yL8DTWc8subkIAS-mWtRkY5TPNowtNCffAh4TMrjkEI';
+
+const headers = {
+  Authorization: TOKEN
+}
+
+const MOCK_HERO_CREATE = {
+  nome: 'Chapolin Colorado',
+  poder: 'Marreta Biônica'
+}
+
+const MOCK_HERO_UPDATE = {
+  nome: 'Hawkeye',
+  poder: 'Mira'
+}
+
+let MOCK_ID = '';
+
+describe('Heroes API test', function () {
+  this.beforeAll(async () => {
+    app = await api;
+    const addToUpdate = await app.inject({
+      method: 'POST',
+      url: '/herois',
+      headers,
+      payload: JSON.stringify(MOCK_HERO_UPDATE)
+    });
+    const updateData = JSON.parse(addToUpdate.payload);
+    MOCK_ID = updateData._id;
+  })
+  it('Read heroes by /herois', async() => {
+    const result = await app.inject({
+      method: 'GET',
+      headers,
+      url: '/herois'
+    });
+
+    const dados = JSON.parse(result.payload);
+    const statusCode = result.statusCode;
+    assert.deepEqual(statusCode, 200);
+    assert.ok(Array.isArray(dados));
+  })
+  it('Read only 3 heroes', async () => {
+    const SIZE_LIMIT = 3;
+    const result = await app.inject({
+      method: 'GET',
+      headers,
+      url: `/herois?skip=0&limit=${SIZE_LIMIT}`
+    })
+    const dados = JSON.parse(result.payload);
+    const statusCode = result.statusCode;
+    assert.deepEqual(statusCode, 200);
+    assert.ok(dados.length === SIZE_LIMIT);
+  })
+  it('Should return a error with wrong "limit"', async () => {
+    const SIZE_LIMIT = 'to-the-moon';
+    const result = await app.inject({
+      method: 'GET',
+      headers,
+      url: `/herois?skip=0&limit=${SIZE_LIMIT}`
+    });
+
+    const statusCode = result.statusCode;
+    assert.deepEqual(statusCode, 400);
+  })
+  it('Should read only 1 hero', async () => {
+    const NAME = MOCK_HERO_UPDATE.nome;
+    const result = await app.inject({
+      method: 'GET',
+      url: `/herois?skip=0&nome=${NAME}`,
+      headers
+    })
+    const dados = JSON.parse(result.payload);
+    const statusCode = result.statusCode;
+    assert.deepEqual(statusCode, 200);
+    assert.ok(dados[0].nome === NAME);
+  })
+  it('Should create hero', async () => {
+    const result = await app.inject({
+      method: 'POST',
+      url: '/herois',
+      headers,
+      payload: MOCK_HERO_CREATE
+    });
+
+    const statusCode = result.statusCode;
+    const { message, _id } = JSON.parse(result.payload);
+    assert.ok(statusCode === 201);
+    assert.notStrictEqual(_id, undefined);
+    assert.deepEqual(message, 'Successfully created new hero!')
+  });
+  it('Should update hero using PATCH method', async () => {
+    const expected = {
+      poder: 'Super mira'
+    }
+    const result = await app.inject({
+      method: 'PATCH',
+      url: `/herois/${MOCK_ID}`,
+      headers,
+      payload: JSON.stringify(expected)
+    });
+
+    const data = JSON.parse(result.payload);
+    assert.ok(result.statusCode === 200);
+    assert.deepEqual(data.message, 'Successfully updated hero');
+  });
+  it('Should fail update hero using PATCH method using incorrect id', async () => {
+    const expected = {
+      poder: 'Super mira'
+    }
+
+    const result = await app.inject({
+      method: 'PATCH',
+      url: `/herois/ca421358131e`,
+      headers,
+      payload: JSON.stringify(expected)
+    });
+
+    const data = JSON.parse(result.payload);
+    assert.ok(result.statusCode === 412);
+    assert.deepEqual(data.message, 'Not allowed update. No donuts for you');
+  });
+  it('Should delete hero using DELETE method by id', async () => {
+    const result = await app.inject({
+      method: 'DELETE',
+      headers,
+      url: `/herois/${MOCK_ID}`
+    })
+    const { statusCode } = result;
+    const data = JSON.parse(result.payload);
+    assert.ok(statusCode === 200);
+    assert.deepEqual(data.message, 'Successfully deleted hero');
+  });
+  it('Should fail delete hero using incorrect id', async () => {
+    const result = await app.inject({
+      method: 'DELETE',
+      headers,
+      url: `/herois/ca421358131e`
+    });
+
+    const data = JSON.parse(result.payload);
+    assert.ok(result.statusCode === 412);
+    assert.deepEqual(data.message, 'Not allowed remove hero. Too strong to delete');
+  });
+  it('Should fail delete hero using incorrect id', async () => {
+    const result = await app.inject({
+      method: 'DELETE',
+      headers,
+      url: `/herois/ID_INVALIDO`
+    });
+
+    const data = JSON.parse(result.payload);
+    const expected = {
+      statusCode: 500,
+      error: 'Internal Server Error',
+      message: 'An internal server error occurred'
+    };
+
+    assert.ok(result.statusCode === 500);
+    assert.deepEqual(data, expected);
+  });
+})
